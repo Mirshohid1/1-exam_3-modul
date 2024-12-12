3086e501195f2a7ecb04665d2fb68e7381369ec8
l0n3r <mirshohid1214@gmail.com>
2024-12-12 10:02:12 +0500

    created model Sensor

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
-from datetime import date
+from datetime import date, datetime
 
 
 class Vehicle(models.Model):
@@ -29,3 +29,30 @@ class Vehicle(models.Model):
 
     def __str__(self):
         return f"Model: {self.model}, Year: {self.year}."
+
+
+class Sensor(models.Model):
+    """
+    This model represents the fields of the car sensor.
+    """
+
+    type = models.CharField(
+        max_length=255,
+        validators=[MinLengthValidator(3)],
+    )
+
+    vehicle = models.ForeignKey(
+        Vehicle,
+        on_delete=models.CASCADE,
+        related_name="sensors"
+    )
+
+    installed_date = models.DateField(
+        validators=[
+            MinValueValidator(date(1986, 1, 1)),
+            MaxValueValidator(date.today()),
+        ]
+    )
+
+    def __str__(self):
+        return f"Sensor type: {self.type} for a vehicle: {self.vehicle.model}."
