62eaefc63aae33ec4df5bd0cbe7edce3b074291a
l0n3r <mirshohid1214@gmail.com>
2024-12-12 09:07:44 +0500

    Created model Vehicle

--- api/models.py
+++ api/models.py
@@ -1,3 +1,31 @@
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from datetime import date
+
+
+class Vehicle(models.Model):
+    """
+    The model representing the vehicle.
+    """
+    
+    model = models.CharField(
+        max_length=255,
+        validators=[MinLengthValidator(3)]
+    )
+    year = models.IntegerField(
+        validators=[
+            MinValueValidator(1700),
+            MaxValueValidator(date.today().year)
+        ]
+    )
+    vin = models.CharField(
+        max_length=17,
+        validators=[
+            MinLengthValidator(17),
+        ],
+        unique=True,
+    )
+
+    def __str__(self):
+        return f"Model: {self.model}, Year: {self.year}."
